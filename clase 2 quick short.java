9845869c2ba0f57a8f2e0ab88f43dbecf2481258
aaho2000 <beatsmaster012@gmail.com>
2025-10-29 12:21:20 -0500

    Actualizar clase 2 quick short.java

--- clase 2 quick short.java
+++ clase 2 quick short.java
@@ -3,7 +3,7 @@ import java.util.Arrays;
 public class QuickSortNombres {
 
     
-      //Método principal de Quick Sort.
+      //Método de Quick Sort principal 
    
     public static void quickSort(String[] nombres, int bajo, int alto) {
         if (bajo < alto) {
